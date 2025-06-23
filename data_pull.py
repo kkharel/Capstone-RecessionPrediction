20c946fc9bc2655c9ee26c2a5bde40c2986c2912
Kushal Kharel <59852121+kkharel@users.noreply.github.com>
2025-06-23 06:13:28 +0000

    sttil

--- data_pull.py
+++ data_pull.py
@@ -7,7 +7,7 @@ import requests
 # from project_config import API_KEY
 import streamlit as st
 
-API_KEY = st.secrets["API_KEY"]
+API_KEY = st.secrets['API_KEY']
 
 logging.basicConfig(level = logging.INFO, format = "%(asctime)s — %(levelname)s — %(message)s")
 logger = logging.getLogger(__name__)
